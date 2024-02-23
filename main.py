c2477365e7ee4db6deb87fdc7839d90f808a8ad7
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-02-23 07:59:26 +0100

    add1 to main.py for importing libraries and download punctuation fron nltk

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from contextlib import asynccontextmanager
+import re
+import tensorflow as tf
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+import pickle
+import nltk
+from nltk.corpus import stopwords
+from pydantic import BaseModel
+from unidecode import unidecode
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
 
 app = FastAPI()
 
