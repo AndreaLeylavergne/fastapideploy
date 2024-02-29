2d9760dfed12f443985c3862e86a8290c4dc4ef8
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-02-29 07:01:28 +0100

    testing import tensorflow and  HTTPException

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import tensorflow as tf
-from keras.models import load_model
-from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import Tokenizer
+
 
 app = FastAPI()
 @app.get("/")
 async def root():
-    return {"message": "Hello, honey world 3.9"}
+    return {"message": "Hello, honey World 3.9"}
