4700c0157dff1b0e4f1f5deac38d38059bcd5987
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-03-01 08:10:01 +0100

    adding contextlib

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-
+from contextlib import asynccontextmanager
 
 
 app = FastAPI()
+
 @app.get("/")
 async def root():
     return {"message": "Hello, honey World 3.9"}
